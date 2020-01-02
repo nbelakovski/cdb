9d0a4c51d330582e13ae203f2ba99f5a939d79f5
Nickolai Belakovski <nbelakovski@gmail.com>
2020-01-01 22:51:02 -0800

    Rely on system installed catch instead of having it in repo

--- src/test_cdb.cpp
+++ src/test_cdb.cpp
@@ -1,6 +1,6 @@
 #include <cdb/cdb.h>
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch2/catch.hpp>
+#include <catch/catch.hpp>
 
 
 TEST_CASE("cdb", "ExtractVars")
